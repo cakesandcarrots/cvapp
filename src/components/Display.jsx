6fb7d5039b68f12e8570827d0610a8ad119f93ff
Akash <veisthebest3@gmail.com>
2023-12-26 12:25:45 +0530

    creating the cv display

---
+++ src/components/Display.jsx
@@ -0,0 +1,13 @@
+function Display(props) {
+
+    return (
+        <>
+            <div className="detailheading"></div>
+            <div>props.name</div>
+
+
+        </>
+    )
+}
+
+export default Display
--- src/components/Geninfo.jsx
+++ src/components/Geninfo.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react"
+import Display from "./Display"
 function Geninfo() {
+
     const [name, setName] = useState("")
     const [mailid, setMaiid] = useState("")
     const [phoneNo, setPhoneNo] = useState("")
+
+    function handlesubmit(e) {
+
+        e.preventDefault()
+        console.log(name)
+        console.log(mailid)
+        console.log(phoneNo)
+
+    }
     return (
         < div >
-            <form action="get">
+            <form onSubmit={handlesubmit}>
                 <fieldset>
                     <legend>Add General Infomation</legend>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" id="name" onChange={(e) => setName(e.target.event)} />
+                    <input type="text" name="name" id="name" onChange={(e) => setName(e.target.value)} />
                     <br />
                     <label htmlFor="email">Email ID:</label>
-                    <input type="email" name="email" id="email" onChange={(e) => setMaiid(e.target.event)} />
+                    <input type="email" name="email" id="email" onChange={(e) => setMaiid(e.target.value)} />
                     <br />
                     <label htmlFor="phoneno">Phone No: </label>
-                    <input type="tel" name="phoneno" id="phoneno" onChange={(e) => setPhoneNo(e.target.event)} />
+                    <input type="tel" name="phoneno" id="phoneno" onChange={(e) => setPhoneNo(e.target.value)} />
                     <br />
-                    <button>Submit</button>
+                    <button type="submit">Submit</button>
 
                 </fieldset>
 
